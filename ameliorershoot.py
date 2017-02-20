4fdf81440acc0590e3f432ee57345d82087bb2b4
BROUX <3607296@ppti-14-303-10.ufr-info-p6.jussieu.fr>
2017-02-20 19:34:03 +0100

    Ajout de fct observ qui test les param de mes fonctions pour le shoot. 20/02

--- ameliorershoot.py
+++ ameliorershoot.py
@@ -1,33 +1,28 @@
-from soccersimulator import SoccerTeam, Simulation, SoccerState, Vector2D, SoccerAction, Strategy
-from soccersimulator import SimuGUI,show_state, show_simu
+from soccersimulator import SoccerTeam, Simulation, Vector2D, SoccerAction, Strategy
+from soccersimulator import show_simu
 import random, math
 from soccersimulator.settings import *
-from toolbox import MyState, Action
+from toolbox import MyState
+import numpy as np
 
-
-def exp(b,a,x):
+def expf(a,b,x):
     return(b*(1-math.exp(-a*x)))
 
-def xexpalpha(b,a,x):
+def xexpalpha(a,b,x):
     return(b*x**a)
 
-def shoot(fct,mystate):
+def shootf(fct,mystate):
     return SoccerAction(Vector2D(),(mystate.position_but_adv-mystate.my_position).normalize()*fct)
-
 
 class shoot(Strategy):
     def __init__(self):
         Strategy.__init__(self,"ElMatador")
-        self.mydic = dict()
-        self.mydic["c"] = 0
+        self.a = 0
+        self.b = 0
     def compute_strategy(self,state,id_team,id_player):
         mystate= MyState(state,id_team,id_player)
-        action= Action(mystate)
-        if self.mydic["c"]==0:
-            return SoccerAction((mystate.ball_position-mystate.my_position).norm_max(0.01),Vector2D())
-        self.mydic["c"]+=1
         x=mystate.my_position.distance(mystate.position_but_adv)
-        return shoot(exp(2,1,x),mystate)
+        return shootf(expf(self.a,self.b,x),mystate)
 
 team1 = SoccerTeam(name="France",login="Paul")
 team2 = SoccerTeam(name="Liban",login="Hassan")
@@ -35,21 +30,57 @@ team1.add("Paul",shoot())
 team2.add("Hassan",shoot())
 
 
-def get_team(i):
-    if i==1:
-        return team1
-    if i==2:
-        return team2
-    if i==4:
-        return team4
 
-
-
-state=SoccerState.create_initial_state(1,0)
-state.player_state(1,0).position=Vector2D(GAME_WIDTH/2+random.uniform(0,1)*GAME_WIDTH/2,random.uniform(0,1)*GAME_WIDTH/2)
-state.ball.position=state.player_state(1,0).position
-
-simu = Simulation(team1,team2,50,state)
-#Jouer et afficher la partie
-show_simu(simu)
-
+# Class Obeserver
+class Observer(object):
+    MAX_STEP=40
+    def __init__(self,visu = True):  # Visu permet la visualisation de la simu. Si on en veut pas, on met false. Cela est permis par la fonction start en bas.
+        team1 = SoccerTeam("expe1")
+        team2 = SoccerTeam("expe2")
+        self.strat = shoot()         # On donne la strat qu'on va utiliser.
+        team1.add("jexp1", self.strat )
+        team2.add("jplot",Strategy())  # Si besoin d'un joueur de team adverse
+        self.simu = Simulation(team1,team2,max_steps=10000000) # On def la simu avec un enorme max_steps car on veut test x round et on veut pas devoir recommencer un match
+        self.visu = visu
+        self.simu.listeners+=self #ajout de l observer
+        list_a = np.linspace(0.1,20,10)    # Creation de la matrice pour la parametre a. De param1 a param2 avec param2 valeurs
+        list_b = np.linspace(0.1,20,10)
+        self.list_params = [(a,b) for a in list_a for b in list_b]   # Creation de tout les couples possible
+        self.cpt_params = 0     # Va permettre de tester toute la liste de couple de params
+        self.nb_expe = 20       # Nb de round que l on fera par postion 
+        self.res = dict()       # Ini de notre dico
+        self.pos = dict()
+    def begin_match(self,team1,team2,state):
+    #initialisation des parametres ...
+        self.last, self.but, self.expe = 0, 0, 0
+    def begin_round(self,team1,team2,state):
+        self.x,self.y = 3*GAME_WIDTH/4+random.uniform(0,1)*GAME_WIDTH/4,random.uniform(0,1)*GAME_HEIGHT/2  # Ini des postions qu on voudra en depart, dans un secteur donne
+        self.simu.state.states[(1,0)].position = Vector2D(self.x,self.y)
+        self.simu.state.ball.position = Vector2D(self.x,self.y)
+    #ou self.simu.set_state(state)
+        self.strat.a,self.strat.b = self.list_params[self.cpt_params]  # On met les vals de a et b dans shoot que l on veut pour les couples dans list_params
+        self.last = self.simu.step   # Pas a la fin du round precedant
+    def update_round(self,team1,team2,state):
+        if state.step>self.last+self.MAX_STEP: 
+            self.simu.end_round()
+    def end_round(self,team1,team2,state):
+        if state.goal>0:    # Si but marque, on incremente but
+            self.but+=1
+        self.expe+=1        # On increment pour chaque round
+        if self.expe > self.nb_expe:
+            if self.cpt_params <len(self.list_params)-1:   # Si on a pas traite tout les couples 
+                self.res[self.list_params[self.cpt_params]]= self.but*1./self.expe  # On met dans res la proba
+                self.pos[self.list_params[self.cpt_params]]= self.x,self.y
+                self.cpt_params+=1  # On change les params qu on va tester
+                self.last, self.but, self.expe = 0, 0, 0    # On reinitialise
+            else:
+                self.simu.end_match()       # Sinon on end
+    def start(self):
+        if self.visu :
+            show_simu(self.simu)
+        else:
+            self.simu.start()
+                    
+    
+    
+# Trouver parametre
